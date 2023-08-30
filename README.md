7c806cf80b51158f559457c7d3bcb10d7fade51c
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2023-08-31 00:24:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fonctionnalités
+Authentification
+
+Création de compte
+
+Envoie de formulaire de contact
+
+Tableau de bord admin
+
+# Reste à faire
+
+Role authentification (admin/client)
+
+validation avant paiement / intégration de stripe
+
+Gestion des évènements type points pour le système de fidélisation
---
+++ components/Team/persons.ts
@@ -0,0 +1,29 @@
+export const persons = [
+    {
+      img: "/images/team/im1.jpg",
+      title:
+        "Equipe de Titovidéo",
+      name: "",
+    },
+    {
+      img: "/images/team/im1.jpg",
+      title: "CEO",
+      name: "Francesca Assemien",
+    },
+    {
+      img: "/images/team/im2.jpg",
+      title: "Community Manager",
+      name: "Léonie Meunier",
+    },
+    {
+      img: "/images/team/im3.png",
+      title: "Développeur Web",
+      name: "Marvin Houguet",
+    },
+    {
+      img: "/images/team/im1.jpg",
+      title: "Equipe de Titovidéo",
+      name: "",
+    },
+  
+  ];
